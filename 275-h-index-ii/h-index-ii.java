c0159b2668d44f034f86bb249d863f861baa6f08
varsharaodevaraj <varsharaodevaraj@gmail.com>
2025-05-29 01:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (61.92%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        //using binary search on possible answers range
-        //[1,3,5,80,200]
-        //H index can be 0,1,2,3,4,5 for 5 length array (basically 0 to n)
+        int n = citations.length; 
+
         int l = 1;
         int r = n;
-        int midIdx = 0;
-        int hIdx = 0;
-        while (l<=r){
-            midIdx = (l + r)/2; //possible h index
-            if (citations[n - midIdx] >= midIdx ) {
-                l = midIdx + 1;
-                hIdx = midIdx;
-            } else{
-                r = midIdx - 1;
+
+        int max = 0;
+
+        while(l<=r){
+            int mid = l + (r-l)/2;
+
+            if(citations[n-mid]>=mid){
+                max = mid;
+                l = mid + 1;
+            }else{
+                r = mid - 1;
             }
         }
-        return hIdx;
+
+        return max;
     }
 }
