33bf291afddd2c78220e7e54d364cf943ef2cbfb
varsharaodevaraj <varsharaodevaraj@gmail.com>
2025-06-04 12:11:36 +0530

    Time: 1 ms (99.73%) | Memory: 54.8 MB (98.01%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,39 +1,26 @@
 class Solution {
-    public boolean isValid(int[] nums,int mid,int target){
-        int n = nums.length;
-        int sum = 0;
-        for(int i=0;i<mid;i++){
-            sum += nums[i];
-        }
-
-        if(sum>=target) return true;
-
-        for(int i=mid;i<n;i++){
-            sum += nums[i] - nums[i-mid];
-            if(sum>=target) return true;
-        }
-
-        return false;
-    }
     public int minSubArrayLen(int target, int[] nums) {
-        // A Binary search approach of time complexity --> O(n log n):
+
+        int l = 0;
+        int r = 0;
 
         int n = nums.length;
-        
-        int l = 0;
-        int r = n;
-        int ans = 0;
 
-        while(l<=r){
-            int mid = l + (r-l)/2;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
 
-            if(isValid(nums,mid,target)){
-                ans = mid;
-                r = mid - 1;
-            }else{
-                l = mid + 1;
+        while(l<n){
+            sum += nums[l];
+
+            while(sum>=target){
+                ans = Math.min(ans,(l-r+1));
+                sum -= nums[r];
+                r++;
             }
-        }
-        return ans;
+
+            l++;
+        } 
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
