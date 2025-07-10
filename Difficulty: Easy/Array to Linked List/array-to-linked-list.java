c69e3dc8fcd5ccf52a5fc90f0563008c1e5cb615
varsharaodevaraj <varsharaodevaraj@gmail.com>
2025-07-10 18:54:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        if(arr == null || arr.length == 0) return null;
+        
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        
+        for(int i=1;i<arr.length;i++){
+            
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = mover.next;
+            
+        }
+        
+        return head;
+    }
+}
