6d9ef3b703247f921a9d8867a3b8d00130aa158f
varsharaodevaraj <varsharaodevaraj@gmail.com>
2025-07-11 12:13:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.java
@@ -0,0 +1,36 @@
+/*
+class DLLNode {
+    int data;
+    DLLNode next;
+    DLLNode prev;
+
+    DLLNode(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public DLLNode reverseDLL(DLLNode head) {
+        // Your code here
+        DLLNode curr = head;
+        DLLNode temp = null;
+        
+        while(curr!=null){
+            
+            temp = curr.prev;
+            curr.prev = curr.next;
+            curr.next = temp;
+            
+            curr = curr.prev;
+            
+        }
+        
+        if(temp!=null){
+            head = temp.prev;
+        }
+        
+        return head;
+    }
+}
