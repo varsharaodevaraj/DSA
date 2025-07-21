74fe5d1823568a2e0b30c101f7dc859b28671543
Varsha Rao <varsharaodevaraj@gmail.com>
2025-07-21 14:21:36 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (84.33%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,52 +2,29 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int n = (n1+n2);
-        int cnt = 0;
-        int idx1 = n/2;
-        int idx2 = idx1-1;
-        int i = 0;
-        int j = 0;
-
-        int ans1 = -1;
-        int ans2 = -1;
-
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(cnt == idx1) ans1 = nums1[i];
-                else if(cnt == idx2) ans2 = nums1[i];
-
-                cnt++;
-                i++;
-            }else{
-                if(cnt == idx1) ans1 = nums2[j];
-                else if(cnt == idx2) ans2 = nums2[j];
-
-                cnt++;
-                j++;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int l = 0;
+        int r  = n1;
+        int left = (n1+n2+1)/2;
+        int n = n1 + n2;
+        while(l<=r){
+            int mid1 = (l+r)/2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if(mid1<n1) r1 = nums1[mid1];
+            if(mid2<n2) r2 = nums2[mid2];
+            if(mid1-1>=0) l1 = nums1[mid1-1];
+            if(mid2-1>=0) l2 = nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1) return Math.max(l1,l2);
+                return ((double)(Math.max(l1,l2)+Math.min(r1,r2)))/2.0;
             }
+            else if(l1>r2) r = mid1-1;
+            else l = mid1+1;
         }
-
-        while(i<n1){
-            if(cnt == idx1) ans1 = nums1[i];
-            else if(cnt == idx2) ans2 = nums1[i];
-
-            cnt++;
-            i++;
-        }
-
-        while(j<n2){
-            if(cnt == idx1) ans1 = nums2[j];
-            else if(cnt == idx2) ans2 = nums2[j];
-
-            cnt++;
-            j++;
-        }
-
-        if(n%2==1){
-            return ans1;
-        }
-
-        return (double)((double)(ans1+ans2))/2.0;
+        return 0;
     }
 }
