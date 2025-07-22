b8869d8302376091f1506e1afdf35ad25e7b2738
Varsha Rao <varsharaodevaraj@gmail.com>
2025-07-22 16:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.19%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return n;
+        int prev2 = 1;
+        int prev1 = 1;
+        int curr = 0;
+        for(int i=2;i<=n;i++){
+            curr = prev2+prev1;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+}
