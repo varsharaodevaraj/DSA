88a00c214a2ce291ee9796eb292d485230f82828
Varsha Rao <varsharaodevaraj@gmail.com>
2025-07-22 16:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=1) return n;
-        int prev2 = 1;
-        int prev1 = 1;
-        int curr = 0;
-        for(int i=2;i<=n;i++){
-            curr = prev2+prev1;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return curr;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
+    public int helper(int n,int[] dp){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
     }
 }
